56f26a79fa6ea5a82eab3b8dc1801ea544cf09aa
shabhavi <123924202+shabhavi@users.noreply.github.com>
2023-02-06 10:51:23 +0530

    Add files via upload

---
+++ problem6.js
@@ -0,0 +1,8 @@
+// data can be anything
+// what if I want to store a name or any other data except number.
+// string..." "
+
+let a=10;// data is number
+let name="goku";// data is string 
+
+console.log(name);
